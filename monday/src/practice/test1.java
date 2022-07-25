3fa8a85e96244c1e7cb3b9a2f0d46e4d92c1c231
SHEETAL <SHEETAL@SHITALPC>
2022-07-25 17:09:03 +0530

    fourth commit

--- monday/src/practice/test1.java
+++ monday/src/practice/test1.java
@@ -6,5 +6,10 @@ public class test1
     {
 		System.out.println("hello.......");
 		System.out.println("good morning");
+		int a=20;
+		int b=40;
+		int sum=a+b;
+		System.out.println(sum);
+		
 	}
 }
---
+++ monday/src/practice/test4.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class test4
+{
+   public static void main(String[] args) 
+   {
+	System.out.println("Good Night");
+}
+}
