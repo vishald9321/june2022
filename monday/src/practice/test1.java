39ba568b91c11a489c401c921fefa8afd963da51
vishald9321 <109948824+vishald9321@users.noreply.github.com>
2022-07-25 17:54:26 +0530

    Update test1.java

--- monday/src/practice/test1.java
+++ monday/src/practice/test1.java
@@ -5,7 +5,7 @@ public class test1
     public static void main(String[] args)
     {
 		System.out.println("hello.......");
-		System.out.println("good morning");
+
 		int a=20;
 		int b=40;
 		int sum=a+b;
